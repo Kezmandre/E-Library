5fab5681f1070d595cb8eecc0b3fcdddbc9920ef
Quadri <kazeemquadri75@gmail.com>
2024-02-12 13:54:20 +0000

    m

--- Backend/server.js
+++ Backend/server.js
@@ -27,10 +27,10 @@ app.use("/shelf", shelfRouter);
 
 const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/Frontend/build")));
+  app.use(express.static(path.join(__dirname, "Frontend/dist")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "Frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
